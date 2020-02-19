adbbc595026835e32df8d1e756a9cd1b7b60941e
Matti Luukkainen <mluukkai@iki.fi>
2020-02-19 10:51:06 +0200

    part8-2

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const { ApolloServer, gql } = require('apollo-server')
+const { ApolloServer, UserInputError, gql } = require('apollo-server')
+
+const uuid = require('uuid/v1')
 
 let persons = [
   {
@@ -41,6 +43,15 @@ const typeDefs = gql`
     allPersons: [Person!]!
     findPerson(name: String!): Person
   }
+
+  type Mutation {
+    addPerson(
+      name: String!
+      phone: String
+      street: String!
+      city: String!
+    ): Person
+  }  
 `
 
 const resolvers = {
@@ -57,7 +68,19 @@ const resolvers = {
         city: root.city
       }
     }
-  }
+  },
+  Mutation: {
+    addPerson: (root, args) => {
+      if (persons.find(p => p.name === args.name)) {
+        throw new UserInputError('Name must be unique', {
+          invalidArgs: args.name,
+        })
+      }
+      const person = { ...args, id: uuid() }
+      persons = persons.concat(person)
+      return person
+    }
+  }  
 }
 
 const server = new ApolloServer({
